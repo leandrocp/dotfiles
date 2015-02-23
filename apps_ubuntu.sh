35064364aeb378885c7ada41bb8eb4746529bd79
Leandro Cesquini Pereira <leandro.cesquini@gmail.com>
2015-02-23 18:24:43 +0000

    Default apps

---
+++ apps_ubuntu.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Ask for the administrator password upfront.
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until the script has finished.
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Make sure we’re using the latest packages
+sudo apt-get update
+
+sudo apt-get install exuberant-ctags -y
