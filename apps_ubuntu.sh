f52e893b9da01e502ad1d680f5feca355b3a4ea1
Leandro Cesquini Pereira <leandro.cesquini@gmail.com>
2015-02-27 13:27:29 +0000

    Mux profiles & the_silver_searcher

--- apps_ubuntu.sh
+++ apps_ubuntu.sh
@@ -10,3 +10,4 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 sudo apt-get update
 
 sudo apt-get install exuberant-ctags -y
+sudo apt-get install silversearcher-ag -y
