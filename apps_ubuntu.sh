2cb2500a0fd5978d83abce02dbf3ee920f37e714
Leandro Cesquini Pereira <leandro.cesquini@gmail.com>
2015-03-05 12:13:04 +0000

    More apps to format code

--- apps_ubuntu.sh
+++ apps_ubuntu.sh
@@ -15,6 +15,8 @@ sudo apt-get install python-dev -y
 sudo apt-get install exuberant-ctags -y
 sudo apt-get install ack-grep -y
 sudo apt-get install silversearcher-ag -y
+sudo apt-get install astyle -y
+sudo apt-get install tidy -y
 
 sudo add-apt-repository ppa:neovim-ppa/unstable -y
 sudo apt-get update -y
